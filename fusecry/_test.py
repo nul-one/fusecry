0efde63c2d34d40cab69a44cedc63a3884a81c7c
Predrag Mandic <predrag@phlogisto.com>
2017-08-12 12:52:26 +0200

    travis tests

---
+++ fusecry/_test.py
@@ -0,0 +1,30 @@
+from fusecry import config, cry
+import os
+import random
+import string
+
+
+## helpers
+
+def random_string(length):
+    return ''.join(random.choice(
+        string.ascii_letters + 
+        string.digits + 
+        string.punctuation + 
+        string.whitespace ) for i in range(length))
+
+
+## tests
+
+def test_cry_enc():
+    c = cry.Cry(random_string(15))
+    data = os.urandom(5000)
+    assert len(c.enc(data)) % config.enc.aes_block == 0
+    assert data != c.enc(data)[:len(data)]
+    assert c.enc(data) != c.enc(data)
+
+def test_cry_dec():
+    c = cry.Cry(random_string(15))
+    data = os.urandom(5000)
+    assert data == c.dec(c.enc(data))[:len(data)]
+
