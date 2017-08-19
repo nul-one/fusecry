e018f1647b5bd9631e40c165c91c846251317327
Predrag Mandic <predrag@phlogisto.com>
2017-08-19 03:32:06 +0200

    version 0.3.0

--- fusecry/__init__.py
+++ fusecry/__init__.py
@@ -2,7 +2,7 @@
 Encrypted filesystem based on FUSE and AES.
 """
 
-__version__ = "0.2.4"
+__version__ = "0.3.0"
 __licence__ = "GPL v3"
 __author__ = "Predrag Mandic"
 __author_email__ = "github@phlogisto.com"
